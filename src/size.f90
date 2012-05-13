f8ad5bf500a2e7eff7d5f63bbeb9cfe8ae6406d6
Jon <komperda.jon@gmail.com>
2012-05-13 14:28:19 -0500

    Addition of size file
    contains global size variables

---
+++ src/size.f90
@@ -0,0 +1,22 @@
+module size
+	integer, parameter             :: xSize      = 240
+	integer, parameter             :: ySize      = 240
+	double precision, parameter    :: Lx	   = 1.0d0
+	double precision, parameter    :: Ly         = 1.0d0
+	double precision, parameter    :: Lz         = 0.10d0
+	
+	!Time Step sizes
+	integer, parameter          :: maxSteps      = 100
+	double precision, parameter :: r             = 0.11d0
+	integer, parameter          :: itmax         = 1000
+	
+	!Fluid domain parameters
+	double precision, parameter :: re            = 100.0d0
+	double precision, parameter :: conv          = 1.0d-5
+	integer, parameter          :: pInterval     = 1
+	
+	!Calculated values
+	double precision, parameter :: dx            = (1/(dble(xSize)-2.0d0))
+	double precision, parameter :: dy            = (1/(dble(ySize)-2.0d0))
+	double precision            :: dt       
+end module size
