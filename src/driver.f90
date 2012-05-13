41e7ffdca134495cd1c697250fc2e94a05847897
Jon <komperda.jon@gmail.com>
2012-05-13 14:05:13 -0500

    Outline of the driver file
    This is the beginning of the code, we have outlined the driver file to
    see what steps are necessary to build the entire code.
    
    It it not yet in working condition, requires the addition of all
    routines called.

---
+++ src/driver.f90
@@ -0,0 +1,67 @@
+program simpleMAC
+	use omp_lib
+	use size
+	
+	implicit none
+	
+	integer						                  :: timestep, t
+	double precision, allocatable, dimension(:,:) :: u, v, p, Fn, Gn, Q
+	
+	!allocate our host memory
+	allocate(  u(xSize,ySize) )
+	allocate(  v(xSize,ySize) )
+	allocate(  p(xSize,ySize) )
+	allocate( Fn(xSize,ySize) )
+	allocate( Gn(xSize,ySize) )
+	allocate(  Q(xSize,ySize) )
+	
+	!establish initial conditions on the host
+	call initialConditions(u,v,p,Fn,Gn,Q)
+	
+	!Our main computational loop
+	do t=1,maxSteps
+		!Calculate our timestep
+		call calcTStep(u,v,t,dt)
+		
+		!Print the timestep to screen / gotta know what's going on
+		write(*,*) 'Timestep: ',t,'dt: ',dt
+		
+		!First boundary condition
+		call ghostCondition(u,v)
+		
+		!calculate Fn and Gn
+		call calcFnGn(u,v,Fn,Gn)
+		
+		!calculate Qn
+		call calcQn(Fn,Gn,Q)
+	
+		!calculate the pressure field
+		call poisson(Q,u,v,t,p)    !Serial Poisson solver (for testing)
+		
+		!calculate u-vel and v-vel
+		call calcVel(Fn,Gn,p,u,v)
+		
+		!Check NaN if case blew up / sucks if it did (>.<)
+		if( isnan(u(int(xSize/2),int(ySize/2))) ) then
+			write(*,*)'Case Blew UP!!!!!!!!!'
+			stop
+		end if
+		
+		!Moving lid boundary
+		call lidCondition(u,v)
+		
+		!Plot to file
+		if(mod(t,pInterval)==0) then 
+			call writeVTK(u,v,p,t)
+		end if
+	end do
+	
+	!free up our memory
+	deallocate(  u )
+	deallocate(  v )
+	deallocate(  p )
+	deallocate( Fn )
+	deallocate( Gn )
+	deallocate(  Q )
+end program macCavity
+	
