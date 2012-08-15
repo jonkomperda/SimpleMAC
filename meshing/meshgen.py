00c9fd0147840487b1f17a88dcfaa578335f63dc
Jon Komperda @Laptop <Komperda.Jon@gmail.com>
2012-08-15 17:44:49 -0500

    addition work on meshing script

--- meshing/meshgen.py
+++ meshing/meshgen.py
@@ -1,5 +1,7 @@
 import sys
 import re
+import pyvtk
+import numpy as ar
 
 # Read in the first line
 def checkHeader():
@@ -17,7 +19,7 @@ def findBlocks():
         if not line.strip():
             continue
         elif "blocks" in line:
-            nBlock = re.findall(r'\d+',line)
+            nBlock = getLineValInt(line)
             print ('Reading '+ str(nBlock) + ' blocks...')
             return nBlock
         else:
@@ -34,25 +36,61 @@ def checkObject():
 
 # if the object is a rectangle then we draw the mesh for a rectangle
 def rectangle():
+    global xMin,yMin,xLeng,yLeng
     print 'Found a rectangle...'
     for line in mesh:
         if not line.strip():
             continue
-        elif "origin" in line:
-            origin = re.findall('\d+.\d+',line.strip('origin:').lstrip())
-            print 'Origin is located at: ' + str(origin)
+        elif "X-origin" in line:
+            xMin = getLineValDouble(line)
             continue
-        elif "x:" in line:
-            xLeng = readLine(line)
-        elif "y:" in line:
-            yLeng = readLine(line)
+        elif "Y-origin" in line:
+            yMin = getLineValDouble(line)
+        elif "X-length" in line:
+            xLeng = getLineValDouble(line)
+        elif "Y-length" in line:
+            yLeng = getLineValDouble(line)
+        elif "X-points" in line:
+            xP = getLineValInt(line)
+        elif "Y-points" in line:
+            yP = getLineValInt(line)
+        elif "end" and "rect" in line:
+            createRect(xMin,yMin,xLeng,yLeng,xP,yP)
 
 def square():
     pass
 
-# Reads everything after the equals sign
-def readLine(line):
-    temp = line.strip
+def createRect(xMin,yMin,xLeng,yLeng,xP,yP):
+    global structure
+    print 'creating a rectangle..' 
+    dx = xLeng / (xP-1)
+    dy = yLeng / (yP-1)
+    xTot = xMin + xLeng + dx
+    yTot = yMin + yLeng + dy
+    x = ar.arange(xMin,xTot,dx)
+    y = ar.arange(yMin,yTot,dy)
+    print x
+    print y
+    
+    ps = [(xp,yp,zp) for xp in x for yp in y for zp in [0]]
+    
+    structure = pyvtk.PolyData(points=ps)
+
+# Strip all unwanted line data and return the value
+def getLineVal(line):
+    temp = line.partition('=')[2]
+    value = temp.strip().rstrip().lstrip()
+    return value
+
+# Returns a stripped int value from the line
+def getLineValInt(line):
+    value = getLineVal(line)
+    return int(value)
+
+# Returns a stripped float value from the line
+def getLineValDouble(line):
+    value = getLineVal(line)
+    return float(value)
     
 
 # These are the possible element types, they are defined in the format
@@ -70,5 +108,9 @@ status = checkHeader()
 blocks = findBlocks()
 
 # Find the first object
-for n in blocks:
+for n in range(0,blocks):
     status = checkObject()
+
+#write the vtk
+vtk = pyvtk.VtkData(structure)
+vtk.tofile('testOut','ascii')
