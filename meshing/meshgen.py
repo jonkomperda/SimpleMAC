28be1af9a442ba91e92204b6a7283eb4949c3c3f
Jon Komperda @Laptop <Komperda.Jon@gmail.com>
2012-08-18 22:20:09 -0500

    now can handle a single block
    can make a structured grid for a single block
    
    need to make multi block support work

--- meshing/meshgen.py
+++ meshing/meshgen.py
@@ -1,7 +1,12 @@
 import sys
+from os import rename, listdir
 import re
 import pyvtk
 import numpy as ar
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+from tkFileDialog import asksaveasfilename as savename
+import subprocess as shell
 
 # Read in the first line
 def checkHeader():
@@ -37,7 +42,7 @@ def checkObject():
 # if the object is a rectangle then we draw the mesh for a rectangle
 def rectangle():
     global xMin,yMin,xLeng,yLeng
-    print 'Found a rectangle...'
+    print 'Shape:   Found a rectangle...'
     for line in mesh:
         if not line.strip():
             continue
@@ -56,25 +61,65 @@ def rectangle():
             yP = getLineValInt(line)
         elif "end" and "rect" in line:
             createRect(xMin,yMin,xLeng,yLeng,xP,yP)
+        elif "bound" in line:
+            continue
+        elif "Points" in line:
+            lineError(line)
+            sys.exit("Keyword error!")
+            sys.exit("You must specify both x and y points")
+        elif "Length" in line:
+            lineError(line)
+            sys.exit("Keyword error!")
+            sys.exit("You must specify both x and y lenghts")
+        else:
+            lineError(line)
+            sys.exit("Invalid keyword specified...")
 
 def square():
-    pass
+    print 'Shape:   Found a square...'
+    for line in mesh:
+        if not line.strip():
+            continue
+        elif "X-origin" in line:
+            xMin = getLineValDouble(line)
+            continue
+        elif "Y-origin" in line:
+            yMin = getLineValDouble(line)
+            continue
+        elif "Length" in line:
+            leng = getLineValDouble(line)
+            continue
+        elif 'X-points' in line:
+            nP = getLineValInt(line)
+            continue
+        elif "end" and "sq" in line:
+            createRect(xMin,yMin,leng,leng,nP,nP)
+        elif "bound" in line:
+            continue
+        elif "X-length" or "Y-length" in line:
+            lineError(line)
+            sys.exit("Keyword error!")
+            sys.exit("A square has equal length sides... =P")
+        else:
+            lineError(line)
+            sys.exit("Invalid keyword specified...")
+            
 
 def createRect(xMin,yMin,xLeng,yLeng,xP,yP):
     global structure
     print 'creating a rectangle..' 
     dx = xLeng / (xP-1)
     dy = yLeng / (yP-1)
-    xTot = xMin + xLeng + dx
-    yTot = yMin + yLeng + dy
+    xTot = xMin + xLeng + dx*0.1
+    yTot = yMin + yLeng + dy*0.1
     x = ar.arange(xMin,xTot,dx)
     y = ar.arange(yMin,yTot,dy)
     print x
     print y
     
-    ps = [(xp,yp,zp) for xp in x for yp in y for zp in [0]]
+    ps = [(xp,yp,zp) for yp in y for xp in x for zp in [0]]
     
-    structure = pyvtk.PolyData(points=ps)
+    structure = pyvtk.StructuredGrid([xP,yP,1],ps)
 
 # Strip all unwanted line data and return the value
 def getLineVal(line):
@@ -91,15 +136,20 @@ def getLineValInt(line):
 def getLineValDouble(line):
     value = getLineVal(line)
     return float(value)
-    
 
+def lineError(line):
+    print "\n\nError in line: "+line
+    
 # These are the possible element types, they are defined in the format
 # 'name' : calledRoutine
 elemType = { 'rect' : rectangle,
              'sq'   : square    }
 
 # Open the mesh file
-mesh = open('cavity.in','r')
+Tk().withdraw()
+filename = askopenfilename()
+#filename = 'cavity.in'
+mesh = open(filename,'r')
 
 # Check the mesh type
 status = checkHeader()
@@ -112,5 +162,24 @@ for n in range(0,blocks):
     status = checkObject()
 
 #write the vtk
+tempFileName = 'temp'
 vtk = pyvtk.VtkData(structure)
-vtk.tofile('testOut','ascii')
+
+#print vtk
+vtk.tofile(tempFileName,'ascii')
+
+#clean the mesh file
+saveFileName = savename()
+tempFile = open(tempFileName+'.vtk','r')
+outFile = open(saveFileName+'.vtk','w+')
+for line in tempFile:
+      if 'int' and 'POINTS' in line:
+            line = line.replace('int','float')
+            outFile.write(line)
+            #print line
+      else:
+            outFile.write(line)
+            #print line
+
+tempFile.close()
+outFile.close()
