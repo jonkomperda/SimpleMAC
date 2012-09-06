879665c38555c84b6c53f9867fa67eab6029b986
aanjum2 <azeam91@gmail.com>
2012-09-06 14:13:04 -0500

    Got rid of the messy code where we copy to a 2d array to determine max value.

--- src/carrier_timeStep.f90
+++ src/carrier_timeStep.f90
@@ -57,47 +57,27 @@
 			!!@todo add parallel processing. it was removed due to do loop replacing maxval(u) and maxval(v) 
 			
 			    !Find Maximum value of u and v in array of elements. 
-			    !maxValU = d(1)%u
-			    !maxValV = d(1)%v
+			    maxValU = d(1)%u
+			    maxValV = d(1)%v
 			    
-			    !do n=2,ySizeSol*xSizeSol
-			       !if(maxValU .lt. d(n)%u) maxValU = d(n)%u
-			       !if(maxValV .lt. d(n)%v) maxValV = d(n)%v
-			    !end do
+			    do n=2,ySizeSol*xSizeSol
+			       if(maxValU .lt. d(n)%u) maxValU = d(n)%u
+			       if(maxValV .lt. d(n)%v) maxValV = d(n)%v
+			    end do
 			    
-			    !do i=1,sides
-			    	!do j=1,sideSize
-			    		!if(maxValU .lt. b(i,j)%u) maxValU = b(i,j)%u
-			    		!if(maxValV .lt. b(i,j)%v) maxValV = b(i,j)%v
-			    	!end do
-			    !end do
-			    
-			    !Calculate using max values determined by above do loop
-				!umax1 = maxvalU 
-				!umax2 = maxval(umax1)
-				!vmax1 = maxvalV
-				!vmax2 = maxval(vmax1)
-				
-				!Copied to 2D Array. THIS IS CHEATING, but it proved my theory that the bug is in this method
-				!I need to learn more about the fortran maxval() function in order to fix this method properly
-				!for element 
-				do n=1,ySizeSol*xSizeSol
-					unew( (d(n)%xLoc(1) + 1) , (d(n)%xLoc(2) +1) ) = d(n)%u
-					vnew( (d(n)%xLoc(1)+1) , (d(n)%xLoc(2)+1)   ) = d(n)%v
-				end do
-				
-				do i=1,sides
+			    do i=1,sides
 			    	do j=1,sideSize
-			    		unew( (b(i,j)%xLoc(1)+1) , (b(i,j)%xLoc(2)+1) ) = b(i,j)%u
-						vnew(( b(i,j)%xLoc(1)+1) , (b(i,j)%xLoc(2)+1) ) = b(i,j)%v
+			    		if(maxValU .lt. b(i,j)%u) maxValU = b(i,j)%u
+			    		if(maxValV .lt. b(i,j)%v) maxValV = b(i,j)%v
 			    	end do
 			    end do
-				
-				umax1 = maxval(unew)
+			    
+			    !Calculate using max values determined by above do loop
+				umax1 = maxvalU 
 				umax2 = maxval(umax1)
-				vmax1 = maxval(vnew)
+				vmax1 = maxvalV
 				vmax2 = maxval(vmax1)
-			
+
 			!Calculate second stability condition
 			dtC = (1.0d0 / r) / (re * (abs(umax2) + abs(vmax2))**2 )
 			
