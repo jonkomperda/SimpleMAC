35171124652580790c9f69d6c9551e9818852c09
Federico <federico.lugnani@gmail.com>
2012-11-02 15:25:10 -0500

    working on connections format in 3D

--- meshing/simplemesh.py
+++ meshing/simplemesh.py
@@ -3,17 +3,18 @@ import meshIn
 
 class simplemesh():
     """it creates the file .mesh"""
-    def __init__(self, points, connect, bc=None):
+    def __init__(self, points, connect, bc=None,):
         
         self.points = points
         self.connect = connect
         self.bc = bc
         
-        self.f = open('final.mesh','w')
+        self.f = open('squaretest.mesh','w')
         
         self.f.write('simple mesh format version = 1.1')
         
         self.nodes()
+        self.connections()
         
     def nodes(self):
         """it prints out the nodes in the right format"""
@@ -27,21 +28,27 @@ class simplemesh():
             p_x = str('%.14f'%i[0])
             p_y = str('%.14f'%i[1])
             p_z = str('%.14f'%i[2])
-            self.f.write('\n' + str(k))
-            #print str(k) + '      ' + p_x + '      ' + p_y + '      ' + p_z
+            self.f.write('\n\t' + str(k) + '\t' + str(p_x) +'\t' + str(p_y) +'\t' + str(p_z))
         
         self.f.write('\nend nodes')
     
     def connections(self):
         """it prints out the connections in the right format"""        
+        
         #print self.connect
         
-        """for j in self.connect:
+        self.f.write('\nelements')
+        
+        k=0
+        
+        for j in self.connect:
+            k=k+1
             p0 = str(j[0])
             p1 = str(j[1])
             p2 = str(j[2])
             p3 = str(j[3])
-            #print p1 + '  ' + p2 + '  ' + p3 + '  ' + p4"""
+            #print p0 + '  ' + p1 + '  ' + p2 + '  ' + p3
+            self.f.write('\n\t' + str(k) + '\t' + str(p0) + '\t' + str(p1) +'\t' + str(p2) +'\t' + str(p3))
         
     
     
@@ -56,6 +63,13 @@ class depth():
         self.lz = lz
         self.npz = npz
         
+        threeDimcase = False
+        
+        if self.npz > 1:
+            threeDimcase = True
+        
+        #print '3D: ' + str(threeDimcase)
+        
         self.dz = self.lz/(self.npz-1)
         self.z_pos = self.points[0][2]
         
@@ -66,20 +80,19 @@ class depth():
             for j in range(self.no_points):
                 #print str(i) + '   ' + str(j)
                 self.points.append((self.points[j][0],self.points[j][1],self.z_pos))
-                
-            
-        #print '%.14f'%self.points[0][1]
-    
-
+        
 
 
 if __name__ == '__main__':
     
-    s1=meshIn.rampquad([-2.0,-2.0],[3.0,-2.0],[-2.0,0.0],[2.0,0.0],3,3)
+    """s1=meshIn.rampquad([-2.0,-2.0],[3.0,-2.0],[-2.0,0.0],[2.0,0.0],3,3)
     s2=meshIn.genshape([3.0,-2.0],[5.0,0.0],[2.0,0.0],[4.0,2.0],3,3)
     
     s = s1 + s2
-    sfinal = depth(s.points,s.connect,2.0,2)
+    sfinal = depth(s.points,s.connect,2.0,2)"""
+    
+    s = meshIn.rectangle(0.0,0.0,2.0,2.0,3,3)
+    sfinal = depth(s.points,s.connect,2.0,3)
     
     simplemesh(sfinal.points,sfinal.connect)
     
