b29c56ad09283b2eaddf67c7c4a1dad15924d3c8
Jon Komperda <komperda.jon@gmail.com>
2012-11-09 11:00:30 -0600

    cleaned up carrier_data file
    to be more legible as well as work with doxygen

--- src/carrier_data.f90
+++ src/carrier_data.f90
@@ -1,8 +1,21 @@
+!! All fluid domain variables are stored within this module.
 module domain
+    !! The element datatype carries all Eulerian frame variables. 
     type element 
-        double precision, dimension(2):: X !<Scaled location coordinates
-        integer, dimension(2)::xLoc !<unScaled location coordinates 
-        double precision:: u, v, p, Fn, Gn, Q !<solution values
-        type (element), pointer:: N, S, E, W !<neighboring elements 
+        ! Position variables
+        double precision, dimension(2)  :: X    !< Position of knot in physical space
+        integer, dimension(2)           :: xLoc !< Position of knot in integer cartesian coordinates (i,j)
+        ! Solution variables
+        double precision                :: u    !< Scalar velocity U in X(1) direction
+        double precision                :: v    !< Scalar velocity V in X(2) direction
+        double precision                :: p    !< Pressure
+        double precision                :: Fn   !< Intermediate variable \f$F_n\f$
+        double precision                :: Gn   !< Intermediate variable \f$G_n\f$
+        double precision                :: Q    !< Intermediate variable \f$Q\f$
+        ! Connectivity variables
+        type (element), pointer         :: N    !< Element connected to the north of current elements location
+        type (element), pointer         :: S    !< Element connected to the south of current elements location
+        type (element), pointer         :: E    !< Element connected to the east of current elements location
+        type (element), pointer         :: W    !< Element connected to the west of current elements location
     end type element 
 end module domain
