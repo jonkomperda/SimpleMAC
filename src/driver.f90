4bfdc41c4f7fdc73df40941ff333b5c18cd918df
Jon Komperda <komperda.jon@gmail.com>
2012-11-09 11:01:31 -0600

    cleaned up driver

--- src/driver.f90
+++ src/driver.f90
@@ -1,62 +1,62 @@
 program simpleMAC
-	use omp_lib
-	use size
-	use domain 
-	implicit none
-	
-	integer						      :: timestep, t
-	type(element), allocatable, Target, dimension(:):: d!<solution domain
-	type(element), allocatable, Target, dimension(:,:):: b!<boundary domains
-	
-	!allocate our host memory
-	allocate(  d(sizeSol) )
-	allocate(  b(sides,boundSideBig) )
+    use omp_lib
+    use size
+    use domain 
+    implicit none
+    
+    integer                           :: timestep, t
+    type(element), allocatable, Target, dimension(:):: d!<solution domain
+    type(element), allocatable, Target, dimension(:,:):: b!<boundary domains
+    
+    !allocate our host memory
+    allocate(  d(sizeSol) )
+    allocate(  b(sides,boundSideBig) )
 
-	!> establish initial conditions on the host
-	call initialConditionsComplexGeometry(d,b)
+    !> establish initial conditions on the host
+    call initialConditionsComplexGeometry(d,b)
 
-	!> Our main computational loop
-	do t=1,maxSteps
+    !> Our main computational loop
+    do t=1,maxSteps
 
-		!> Calculate our timestep
-		call calcTStepComplexGeometry(d,b,t,dt)
-		
-		!> Print the timestep to screen / gotta know what's going on
-		write(*,*) 'Timestep: ',t,'dt: ',dt
-		
-		!> First boundary condition
-		call ghostConditionComplexGeometry(b)
-		
-		!> calculate Fn and Gn
-		call calcFnGnComplexGeometry(d)
-		
-		!> calculate Qn
-		call calcQnComplexGeometry(d)
-		
-		!> calculate the pressure field
-		call poissonComplexGeometry(d,b,t)
-		
-		!> calculate u-vel and v-vel
-		call calcVelComplexGeometry(d)
-		
-		!> Check NaN if case blew up (>.<)
-		!if( isnan(u(int(xSize/2),int(ySize/2))) ) then
-			!write(*,*)'Case Blew UP!!!!!!!!!'
-			!stop
-		!end if
-		
-		!> Moving lid boundary
-		call lidConditionComplexGeometry(b)
-		
-		!> Plot to file
-		if(mod(t,pInterval)==0) then 
-			call writeVTKComplexGeometry(d,t)
-		end if
+        !> Calculate our timestep
+        call calcTStepComplexGeometry(d,b,t,dt)
+        
+        !> Print the timestep to screen / gotta know what's going on
+        write(*,*) 'Timestep: ',t,'dt: ',dt
+        
+        !> First boundary condition
+        call ghostConditionComplexGeometry(b)
+        
+        !> calculate Fn and Gn
+        call calcFnGnComplexGeometry(d)
+        
+        !> calculate Qn
+        call calcQnComplexGeometry(d)
+        
+        !> calculate the pressure field
+        call poissonComplexGeometry(d,b,t)
+        
+        !> calculate u-vel and v-vel
+        call calcVelComplexGeometry(d)
+        
+        !> Check NaN if case blew up (>.<)
+        !if( isnan(u(int(xSize/2),int(ySize/2))) ) then
+            !write(*,*)'Case Blew UP!!!!!!!!!'
+            !stop
+        !end if
+        
+        !> Moving lid boundary
+        call lidConditionComplexGeometry(b)
+        
+        !> Plot to file
+        if(mod(t,pInterval)==0) then 
+            call writeVTKComplexGeometry(d,t)
+        end if
 
-	end do
+    end do
 
-	!> free up our memory
-	deallocate(  d )
-	deallocate(  b )
+    !> free up our memory
+    deallocate(  d )
+    deallocate(  b )
 end program simpleMAC
-	
+    
