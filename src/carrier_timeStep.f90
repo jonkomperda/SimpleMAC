9fc7b80f96fceb92b352329989712fd672718046
Azeam Anjum <azeam.anjum@outlook.com>
2012-11-11 23:56:46 -0600

    Converted Tabs to Spaces using Sublime Text 2
    This was done to help the Doxygen parser work more smoothly.

--- src/carrier_timeStep.f90
+++ src/carrier_timeStep.f90
@@ -1,60 +1,60 @@
-		!> Calculates our timestep for variable timestepping algorithm
-	subroutine calcTStepComplexGeometry(d,b,t,dtNew)
-		use omp_lib
-		use size
-		use domain
-		implicit none
-		double precision, intent(out)					:: dtNew
-		double precision								:: umax1(2),umax2,vmax1(2),vmax2,dtC,dtR, maxValU, maxValV
-		integer, intent(in)								:: t
-		integer                                         :: n,i,j,boundSize
-		type(element), Target, dimension(sizeSol), intent(inout) :: d
-		type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
-		
-		!We use different step sizes depending on how long we've been running
-		if(t .gt. 10) then
-			!Calculate first stability condition
-			dtR = r*dx*dy*re
-			
-			!get the maximum value of u and v using worksharing
-			!!@todo add parallel processing. it was removed due to do loop replacing maxval(u) and maxval(v) 
-			
-			    !Find Maximum value of u and v in array of elements. 
-			    maxValU = d(1)%u
-			    maxValV = d(1)%v
-			    
-			    do n=2,sizeSol
-			       if(maxValU .lt. d(n)%u) maxValU = d(n)%u
-			       if(maxValV .lt. d(n)%v) maxValV = d(n)%v
-			    end do
-			    
-			    do i=1,sides
-			    	if (i == 1 .or. i == 4) then
-						boundSize = boundSideSmall
-					else if (i == 2 .or. i == 3) then
-						boundSize = boundSideSmall + 1
-					else if (i==5 .or. i==6) then
-						boundSize = boundSideBig
-					end if
-			    	do j=1,boundSize
-			    		if(maxValU .lt. b(i,j)%u) maxValU = b(i,j)%u
-			    		if(maxValV .lt. b(i,j)%v) maxValV = b(i,j)%v
-			    	end do
-			    end do
-			    
-			    !Calculate using max values determined by above do loop
-				umax1 = maxvalU 
-				umax2 = maxval(umax1)
-				vmax1 = maxvalV
-				vmax2 = maxval(vmax1)
+        !> Calculates our timestep for variable timestepping algorithm
+    subroutine calcTStepComplexGeometry(d,b,t,dtNew)
+        use omp_lib
+        use size
+        use domain
+        implicit none
+        double precision, intent(out)                   :: dtNew
+        double precision                                :: umax1(2),umax2,vmax1(2),vmax2,dtC,dtR, maxValU, maxValV
+        integer, intent(in)                             :: t
+        integer                                         :: n,i,j,boundSize
+        type(element), Target, dimension(sizeSol), intent(inout) :: d
+        type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
+        
+        !We use different step sizes depending on how long we've been running
+        if(t .gt. 10) then
+            !Calculate first stability condition
+            dtR = r*dx*dy*re
+            
+            !get the maximum value of u and v using worksharing
+            !!@todo add parallel processing. it was removed due to do loop replacing maxval(u) and maxval(v) 
+            
+                !Find Maximum value of u and v in array of elements. 
+                maxValU = d(1)%u
+                maxValV = d(1)%v
+                
+                do n=2,sizeSol
+                   if(maxValU .lt. d(n)%u) maxValU = d(n)%u
+                   if(maxValV .lt. d(n)%v) maxValV = d(n)%v
+                end do
+                
+                do i=1,sides
+                    if (i == 1 .or. i == 4) then
+                        boundSize = boundSideSmall
+                    else if (i == 2 .or. i == 3) then
+                        boundSize = boundSideSmall + 1
+                    else if (i==5 .or. i==6) then
+                        boundSize = boundSideBig
+                    end if
+                    do j=1,boundSize
+                        if(maxValU .lt. b(i,j)%u) maxValU = b(i,j)%u
+                        if(maxValV .lt. b(i,j)%v) maxValV = b(i,j)%v
+                    end do
+                end do
+                
+                !Calculate using max values determined by above do loop
+                umax1 = maxvalU 
+                umax2 = maxval(umax1)
+                vmax1 = maxvalV
+                vmax2 = maxval(vmax1)
 
-			!Calculate second stability condition
-			dtC = (1.0d0 / r) / (re * (abs(umax2) + abs(vmax2))**2 )
-			
-			!use the smaller of the two
-			dtNew = min(dtR,dtC)
-		else
-			!If first few steps then take a very small step
-			dtNew = (r/25.0d0)*dx*dy*re
-		end if
-	end subroutine calcTStepComplexGeometry
+            !Calculate second stability condition
+            dtC = (1.0d0 / r) / (re * (abs(umax2) + abs(vmax2))**2 )
+            
+            !use the smaller of the two
+            dtNew = min(dtR,dtC)
+        else
+            !If first few steps then take a very small step
+            dtNew = (r/25.0d0)*dx*dy*re
+        end if
+    end subroutine calcTStepComplexGeometry
--- src/debug.f90
+++ src/debug.f90
@@ -1,109 +1,109 @@
 subroutine debugPointLocations(d,b)
-	use omp_lib
-	use size
-	use domain
-	implicit none
-	type(element), Target, dimension(sizeSol), intent(inout) :: d
-	type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
-	integer				:: n,i,j,boundSize
+    use omp_lib
+    use size
+    use domain
+    implicit none
+    type(element), Target, dimension(sizeSol), intent(inout) :: d
+    type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
+    integer             :: n,i,j,boundSize
 
-	do n=1,sizeSol
-		write(*,*) 'sol ',n, ' X:', d(n)%xLoc(1), ' Y:', d(n)%xLoc(2)
-	end do
+    do n=1,sizeSol
+        write(*,*) 'sol ',n, ' X:', d(n)%xLoc(1), ' Y:', d(n)%xLoc(2)
+    end do
 
-	do i=1,sides
-		if (i == 1 .or. i == 4) then
-			boundSize = boundSideSmall
-		else if (i == 2 .or. i == 3) then
-			boundSize = boundSideSmall + 1
-		else if (i==5 .or. i==6) then
-			boundSize = boundSideBig
-		end if
-			write(*,*)' '
-			write(*,*) 'Bound ', i
-		do j=1, boundSize
-			write(*,*) 'bound ', j, ' X:', b(i,j)%xLoc(1), ' Y:', b(i,j)%xLoc(2)
-		end do
-	end do
+    do i=1,sides
+        if (i == 1 .or. i == 4) then
+            boundSize = boundSideSmall
+        else if (i == 2 .or. i == 3) then
+            boundSize = boundSideSmall + 1
+        else if (i==5 .or. i==6) then
+            boundSize = boundSideBig
+        end if
+            write(*,*)' '
+            write(*,*) 'Bound ', i
+        do j=1, boundSize
+            write(*,*) 'bound ', j, ' X:', b(i,j)%xLoc(1), ' Y:', b(i,j)%xLoc(2)
+        end do
+    end do
 
 end subroutine debugPointLocations
 
 subroutine debugSolPointConnections(d,b)
-	use omp_lib
-	use size
-	use domain
-	implicit none
-	type(element), Target, dimension(sizeSol), intent(inout) :: d
-	type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
-	integer				:: n,i,j
+    use omp_lib
+    use size
+    use domain
+    implicit none
+    type(element), Target, dimension(sizeSol), intent(inout) :: d
+    type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
+    integer             :: n,i,j
 
-	do n=1,sizeSol
+    do n=1,sizeSol
 
-		if(d(n)%xLoc(1) + 1 /= d(n)%E%xLoc(1) .or.  d(n)%xLoc(1)-1 /= d(n)%W%xLoc(1)) then
-			write(*,*) 'East or West has wrong X cord at: ', n
-		end if
+        if(d(n)%xLoc(1) + 1 /= d(n)%E%xLoc(1) .or.  d(n)%xLoc(1)-1 /= d(n)%W%xLoc(1)) then
+            write(*,*) 'East or West has wrong X cord at: ', n
+        end if
 
-		if(d(n)%xLoc(2) /= d(n)%E%xLoc(2) .or.  d(n)%xLoc(2) /= d(n)%W%xLoc(2)) then
-			write(*,*) 'East or West has wrong Y cord at: ', n
-		end if
+        if(d(n)%xLoc(2) /= d(n)%E%xLoc(2) .or.  d(n)%xLoc(2) /= d(n)%W%xLoc(2)) then
+            write(*,*) 'East or West has wrong Y cord at: ', n
+        end if
 
-		if(d(n)%xLoc(1) /= d(n)%N%xLoc(1) .or.  d(n)%xLoc(1) /= d(n)%S%xLoc(1)) then
-			write(*,*) 'North or South has wrong X cord at: ', n
-		end if
+        if(d(n)%xLoc(1) /= d(n)%N%xLoc(1) .or.  d(n)%xLoc(1) /= d(n)%S%xLoc(1)) then
+            write(*,*) 'North or South has wrong X cord at: ', n
+        end if
 
-		if(d(n)%xLoc(2)+1 /= d(n)%N%xLoc(2) .or.  d(n)%xLoc(2)-1 /= d(n)%S%xLoc(2)) then
-			write(*,*) 'North or South has wrong Y cord at: ', n
-		end if
+        if(d(n)%xLoc(2)+1 /= d(n)%N%xLoc(2) .or.  d(n)%xLoc(2)-1 /= d(n)%S%xLoc(2)) then
+            write(*,*) 'North or South has wrong Y cord at: ', n
+        end if
 
-	end do
+    end do
 
 end subroutine debugSolPointConnections
 
 subroutine debugBoundPointConnections(d,b)
-	use omp_lib
-	use size
-	use domain
-	implicit none
-	type(element), Target, dimension(sizeSol), intent(inout) :: d
-	type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
-	integer				:: n,i,j,boundSize
+    use omp_lib
+    use size
+    use domain
+    implicit none
+    type(element), Target, dimension(sizeSol), intent(inout) :: d
+    type(element), Target, dimension(sides, boundSideBig), intent(inout) :: b
+    integer             :: n,i,j,boundSize
 
-	do i=1,sides
-		if (i == 1 .or. i == 4) then
-			boundSize = boundSideSmall
-		else if (i == 2 .or. i == 3) then
-			boundSize = boundSideSmall + 1
-		else if (i==5 .or. i==6) then
-			boundSize = boundSideBig
-		end if
+    do i=1,sides
+        if (i == 1 .or. i == 4) then
+            boundSize = boundSideSmall
+        else if (i == 2 .or. i == 3) then
+            boundSize = boundSideSmall + 1
+        else if (i==5 .or. i==6) then
+            boundSize = boundSideBig
+        end if
 
-		do j=1, boundSize
-			if(b(i,j)%xLoc(1) /= 0) then
-				if( b(i,j)%xLoc(1)-1 /= b(i,j)%W%xLoc(1) .or. b(i,j)%xLoc(2) /= b(i,j)%W%xLoc(2)) then
-					write(*,*) 'West has wrong cord at: ', i,j
-				end if
-			end if
+        do j=1, boundSize
+            if(b(i,j)%xLoc(1) /= 0) then
+                if( b(i,j)%xLoc(1)-1 /= b(i,j)%W%xLoc(1) .or. b(i,j)%xLoc(2) /= b(i,j)%W%xLoc(2)) then
+                    write(*,*) 'West has wrong cord at: ', i,j
+                end if
+            end if
 
-			if(b(i,j)%xLoc(2) /= 0 .and. (b(i,j)%xLoc(2) /= boundSideSmall .and. b(i,j)%xLoc(1) < solSideSmall + 1)) then
-				if( b(i,j)%xLoc(1) /= b(i,j)%S%xLoc(1) .or. b(i,j)%xLoc(2)-1 /= b(i,j)%S%xLoc(2)) then
-					write(*,*) 'South has wrong cord at: ', i,j
-				end if
-			end if
+            if(b(i,j)%xLoc(2) /= 0 .and. (b(i,j)%xLoc(2) /= boundSideSmall .and. b(i,j)%xLoc(1) < solSideSmall + 1)) then
+                if( b(i,j)%xLoc(1) /= b(i,j)%S%xLoc(1) .or. b(i,j)%xLoc(2)-1 /= b(i,j)%S%xLoc(2)) then
+                    write(*,*) 'South has wrong cord at: ', i,j
+                end if
+            end if
 
-			if(b(i,j)%xLoc(2) /= boundSideBig - 1) then
-				if(b(i,j)%xLoc(1) /= b(i,j)%N%xLoc(1) .or. b(i,j)%xLoc(2) + 1 /= b(i,j)%N%xLoc(2)) then
-					write(*,*) 'North has wrong cord at: ', i,j	
-				end if
-			end if
+            if(b(i,j)%xLoc(2) /= boundSideBig - 1) then
+                if(b(i,j)%xLoc(1) /= b(i,j)%N%xLoc(1) .or. b(i,j)%xLoc(2) + 1 /= b(i,j)%N%xLoc(2)) then
+                    write(*,*) 'North has wrong cord at: ', i,j 
+                end if
+            end if
 
-			if((b(i,j)%xLoc(1)/=boundSideSmall-1 .and. b(i,j)%xLoc(2)<boundSideSmall+1)  & 
-				.and. (b(i,j)%xLoc(2)>boundSideSmall .and. b(i,j)%xLoc(1) /= boundSideBig-1) ) then
-				if(b(i,j)%xLoc(2) /= b(i,j)%E%xLoc(2) .or. b(i,j)%xLoc(1)+1 /= b(i,j)%E%xLoc(1)) then
-					write(*,*) 'East has wrong cord at: ', i,j		
-				end if
-			end if
-		end do
+            if((b(i,j)%xLoc(1)/=boundSideSmall-1 .and. b(i,j)%xLoc(2)<boundSideSmall+1)  & 
+                .and. (b(i,j)%xLoc(2)>boundSideSmall .and. b(i,j)%xLoc(1) /= boundSideBig-1) ) then
+                if(b(i,j)%xLoc(2) /= b(i,j)%E%xLoc(2) .or. b(i,j)%xLoc(1)+1 /= b(i,j)%E%xLoc(1)) then
+                    write(*,*) 'East has wrong cord at: ', i,j      
+                end if
+            end if
+        end do
 
 
-	end do
+    end do
 end subroutine debugBoundPointConnections
