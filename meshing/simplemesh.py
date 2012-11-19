9a7a60f4b1070ed6f98d3d34722f78c0e377fb3e
Federico <federico.lugnani@gmail.com>
2012-11-19 11:51:55 -0600

    corrections about elements numbering

--- meshing/simplemesh.py
+++ meshing/simplemesh.py
@@ -22,9 +22,11 @@ class depth():
         self.connections()
         
         #frontback function is called only for three dimensional cases
-        if self.npz > 1:
+        
+        if self.npz > 2:
             self.bc_faces()
-            self.bc_frontback()
+        
+        self.bc_frontback()
         
     
     
@@ -107,7 +109,9 @@ class depth():
     def bc_frontback(self):
         """it calculates the boundary conditions on front and back faces"""
         
+        self.no_elements = self.no_connections
         self.temp_bc = []
+        
         for i in range(len(self.bc)):
             self.temp_bc.append((self.bc[i][0],self.bc[i][1]+2,self.bc[i][2]))
         
@@ -228,17 +232,37 @@ if __name__ == '__main__':
     #s2 = meshIn.rampquad([2.0,0.0],[4.0,0.0],[2.0,2.0],[6.0,2.0],3,3,(3,2,0,0))
     #s2 = meshIn.square(2.0,0.0,2.0,3,3,(3,2,0,0))
     
-    s1 = meshIn.rectangle(0.0,0.0,4.0,1.0,17,5,(3,0,3,1))
-    s2 = meshIn.rampquad([4.0,0.0],[5.0,0.0],[4.0,1.0],[8.0,1.0],17,5,(3,3,0,0))
-    s3 = meshIn.rampquad([4.0,1.0],[8.0,1.0],[5.0,1.5],[8.0,1.5],17,5,(0,0,3,3))    
-    s4 = meshIn.rectangle(8.0,1.0,4.0,0.5,17,5,(3,2,3,0))    
+    #speeduptest
+    #s1 = meshIn.rectangle(0.0,0.0,4.0,1.0,17,5,(3,0,3,1))
+    #s2 = meshIn.rampquad([4.0,0.0],[5.0,0.0],[4.0,1.0],[8.0,1.0],17,5,(3,3,0,0))
+    #s3 = meshIn.rampquad([4.0,1.0],[8.0,1.0],[5.0,1.5],[8.0,1.5],17,5,(0,0,3,3))    
+    #s4 = meshIn.rectangle(8.0,1.0,4.0,0.5,17,5,(3,2,3,0))    
     
-    sint = s1 + s2 + s3 + s4
+    #pipewithhole
+    """s1 = meshIn.rectangle(0.0,0.0,0.875,0.85,36,18,(2,0,0,2))
+    s2 = meshIn.rectangle(0.875,0.0,0.25,0.85,11,18,(2,0,3,0))
+    s3 = meshIn.rectangle(1.125,0.0,0.875,0.85,36,18,(2,2,0,0))
+    s4 = meshIn.rectangle(1.125,0.85,0.875,0.3,36,7,(0,2,0,3))
+    s5 = meshIn.rectangle(1.125,1.15,0.875,0.85,36,18,(0,2,2,0))
+    s6 = meshIn.rectangle(0.875,1.15,0.25,0.85,11,18,(3,0,2,0))
+    s7 = meshIn.rectangle(0.0,1.15,0.875,0.85,36,18,(0,0,2,2))
+    s8 = meshIn.rectangle(0.0,0.85,0.875,0.3,36,7,(0,3,0,2))"""
+    s1 = meshIn.rectangle(0.0,0.0,0.875,0.85,2,2,(2,0,0,2))
+    s2 = meshIn.rectangle(0.875,0.0,0.25,0.85,2,2,(2,0,3,0))
+    s3 = meshIn.rectangle(1.125,0.0,0.875,0.85,2,2,(2,2,0,0))
+    s4 = meshIn.rectangle(1.125,0.85,0.875,0.3,2,2,(0,2,0,3))
+    s5 = meshIn.rectangle(1.125,1.15,0.875,0.85,2,2,(0,2,2,0))
+    s6 = meshIn.rectangle(0.875,1.15,0.25,0.85,2,2,(3,0,2,0))
+    s7 = meshIn.rectangle(0.0,1.15,0.875,0.85,2,2,(0,0,2,2))
+    s8 = meshIn.rectangle(0.0,0.85,0.875,0.3,2,2,(0,3,0,2))
     
-    s = depth(sint.points,sint.connect,sint.bc,2.0,9,(3,3))
     
-    simplemesh('speeduptest',s.points,s.connect,s.bc,6,6,6)
+    sint = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8
+        
+    s = depth(sint.points,sint.connect,sint.bc,0.1,2,(2,2))
+    
+    simplemesh('pipewithhole',s.points,s.connect,s.bc,1,1,1)
     
     vtk = pyvtk.VtkData(pyvtk.UnstructuredGrid( s.points, hexahedron=s.connect))
-    vtk.tofile('speeduptest')
+    vtk.tofile('pipewithhole')
     
